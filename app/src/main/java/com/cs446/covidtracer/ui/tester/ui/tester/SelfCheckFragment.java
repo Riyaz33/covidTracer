0e1c8fdaa0c48402f6f3407a56774f16a4aee8e2
riyaz33 <Riyaz.shaikh33@icloud.com>
2020-07-16 20:49:32 -0400

    Text changes to self test

--- app/src/main/java/com/cs446/covidtracer/ui/tester/ui/tester/SelfCheckFragment.java
+++ app/src/main/java/com/cs446/covidtracer/ui/tester/ui/tester/SelfCheckFragment.java
@@ -2,7 +2,6 @@ package com.cs446.covidtracer.ui.tester.ui.tester;
 
 import androidx.lifecycle.ViewModelProviders;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -13,17 +12,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.cs446.covidtracer.MainActivity;
 import com.cs446.covidtracer.R;
-
-import static androidx.core.content.ContextCompat.getSystemService;
 
 public class SelfCheckFragment extends Fragment {
 
@@ -33,7 +28,8 @@ public class SelfCheckFragment extends Fragment {
 
     private SelfCheckViewModel selfCheckViewModel;
     private Button quitButton;
-    private TextView disclaimerText;
+    private TextView titleText;
+    private TextView bodyText;
     private Button continueButton;
     private static int stage;
     private RadioGroup yesNo;
@@ -57,15 +53,15 @@ public class SelfCheckFragment extends Fragment {
             }
         });
 
-        disclaimerText = (TextView) root.findViewById(R.id.disclaimer);
-        disclaimerText.setText("Complete this test and you will get a recommendation \n'" +
-                "If this is a  medical emergency, call 911" +
-                "Disclaimer: \n" +
-                "This is not an official COVID-19 test. \n " +
-                "This test will help you diagnose your current situation.\n " +
-                "The test is based on information provided by the Government of Canada COVID-19 Website. \n" +
-                "If you have questions, consult a health care provider. "); // todo: add links to public health providers
+        bodyText = (TextView) root.findViewById(R.id.bodyText);
+        titleText = (TextView) root.findViewById(R.id.disclaimer);
 
+        titleText.setText("Complete this test to do a self-assessment \n'"); // todo: add links to public health providers
+        bodyText.setText("If this is a  medical emergency, call 911\n\n" +
+                "Disclaimer:\n" +
+                "This is not an official COVID-19 test. It will help you diagnose your current situation.\n\n" +
+                "The test is based on information provided by the Government of Canada COVID-19 Website.\n" +
+                "If you have questions, consult a health care provider.");
 
         continueButton = (Button) root.findViewById(R.id.startTest);
 
@@ -126,17 +122,19 @@ public class SelfCheckFragment extends Fragment {
     private void nextQuestion() {
         System.out.println("switch stage " + SelfCheckFragment.stage);
         if (SelfCheckFragment.stage == 0) {
-            ((ViewGroup) disclaimerText.getParent()).addView(yesNo);
+            ((ViewGroup) titleText.getParent()).addView(yesNo);
 
-            disclaimerText.setText("Do you have any of these symptoms? \n " +
-                    "- Tough time breathing \n" +
-                    "- Chest pain\n" +
-                    "- Confusion\n" +
-                    "- Losing consciousness");
+
+            titleText.setText("Do you have any of these symptoms?\n\n");
+            bodyText.setText("- Tough time breathing \n" +
+                            "- Chest pain\n" +
+                            "- Confusion\n" +
+                            "- Losing consciousness");
             continueButton.setText("Continue");
         } else if (SelfCheckFragment.stage == 1) {
-            disclaimerText.setText("Do you have any of these symptoms? \n " +
-                    "- Cough\n" +
+            titleText.setText("Do you have any of these symptoms? \n ");
+
+            bodyText.setText("- Cough\n" +
                     "- Pink eye \n" +
                     "- Fever\n" +
                     "- Chills \n" +
@@ -145,31 +143,33 @@ public class SelfCheckFragment extends Fragment {
             yesNo.clearCheck();
         } else if (SelfCheckFragment.stage == 2 && SelfCheckFragment.sick ==0){
             // ask age
-            disclaimerText.setText("What is your age? \n" +
-                    "You may leave this blank.\n" +
-                    "Leaving it blank will affect the accuracy of the test" );
+            titleText.setText("What is your age?");
+            bodyText.setText("You may leave this blank. Leaving it blank will affect the accuracy of the test" );
             ((ViewGroup) yesNo.getParent()).addView(age);
             ((ViewGroup) yesNo.getParent()).removeView(yesNo);
 
         } else if (SelfCheckFragment.stage == -1) {
             System.out.println("case -1 " + SelfCheckFragment.stage);
-            disclaimerText.setText("Please visit your local health care provider ASAP.");
+            titleText.setText("Please visit your local health care provider ASAP.");
             ((ViewGroup) yesNo.getParent()).removeView(yesNo);
             ((ViewGroup) continueButton.getParent()).removeView(continueButton);
+            ((ViewGroup) quitButton.getParent()).removeView(bodyText);
         } else if (SelfCheckFragment.sick == 1) {
             age.setInputType(0);
 
-            disclaimerText.setText("You are showing some symptoms of COVID-19.\n\n" +
-                    "We recommend you should get an official COVID-19 Test.\n" +
-                    "Click here to find out how you can get tested");//todo
+            titleText.setText("You are showing some symptoms of COVID-19.");
+            bodyText.setText("\nWe recommend you to get an official COVID-19 Test.\n\n" +
+                    "Click here to find out how you can get tested");
+
             ((ViewGroup) yesNo.getParent()).removeView(yesNo);
             ((ViewGroup) continueButton.getParent()).removeView(continueButton);
+
+
         }  else if(SelfCheckFragment.stage == 3){
             age.setInputType(0);
-            disclaimerText.setText("You are in good health. \n\n" +
-                    "Stay healthy by practicing social distancing, wearing a mask, and washing your hands frequently");
+            titleText.setText("You are not at risk for COVID-19.");
+            bodyText.setText("Stay healthy by practicing social distancing, wearing a mask, and washing your hands frequently.");
             ((ViewGroup) continueButton.getParent()).removeView(continueButton);
-
             ((ViewGroup) age.getParent()).removeView(age);
 
         }
